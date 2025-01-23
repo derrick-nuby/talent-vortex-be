2109dd185edb84ed8f5ac00d6bf7a70e01bd69d7
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-23 15:58:39 +0200

    ft: implement jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,36 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from "../../user/schemas/user.schema";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET')
+    });
+  }
+
+  async validate(payload: { sub: string; role: string }) {
+    const user = await this.userModel.findById(payload.sub).exec();
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role
+    };
+  }
+}
