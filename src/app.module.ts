8ebb735cb05688b7a14bf5c51232d1b0ca0946de
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-23 15:07:27 +0200

    ft: add auth and mail modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { MailModule } from './mail/mail.module';
+import { MailModule } from "./mail/mail.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
     MongooseModule.forRootAsync({
       imports: [ ConfigModule, UserModule ],
       useFactory: async (configService: ConfigService) => ({
@@ -19,7 +17,8 @@ import { MailModule } from './mail/mail.module';
       inject: [ConfigService]
     }),
     UserModule,
-    MailModule
+    MailModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
