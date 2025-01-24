00baadb21f00901e3e6804538847e97653799da2
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-24 17:29:04 +0200

    ft: add pagination response

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface PaginationResponse<T> {
+  data: T[];
+  pagination: {
+    totalItems: number;
+    itemCount: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  };
+}
