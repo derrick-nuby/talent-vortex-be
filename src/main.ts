303c3fa96f49eafbae602dd8b49b79842837d1ef
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-24 14:19:29 +0200

    ft: configured swagger api-docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
+
+  const apiPrefix = 'api';
+
+  const config = new DocumentBuilder()
+    .setTitle('Talent Vortex API')
+    .setDescription('Talent API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000', 'Local Development Server')
+    .addServer('', 'Production Server')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT Token',
+      in: 'header'
+    }, 'JWT-auth')
+    .build()
+
   const app = await NestFactory.create(AppModule);
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  app.setGlobalPrefix(apiPrefix, {
+    exclude: ['/']
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
