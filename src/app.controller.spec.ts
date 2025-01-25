15abe486eee9ef46f10196e427bc9d6c2c4d9ead
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-25 21:11:01 +0200

    ft: add challenge and category modules and swagger docs to default route

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('Welcome to talent vortex API v.1.0"', () => {
+      expect(appController.getHello()).toBe('Welcome to talent vortex API v.1.0');
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,22 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ChallengeModule } from './challenge/challenge.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true }),
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
+      imports: [ConfigModule, ChallengeModule, CategoryModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
     }),
-
+    ChallengeModule,
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
