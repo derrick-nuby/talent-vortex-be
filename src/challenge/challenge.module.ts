ae8fa83332b9df6a3bacf9682cd501c43cefff78
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-26 00:13:45 +0200

    refactor: add mongodb object id filter type

--- src/challenge/challenge.module.ts
+++ src/challenge/challenge.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ChallengeService } from './challenge.service';
-import { ChallengeController } from './challenge.controller';
+import { Module } from "@nestjs/common";
+import { ChallengeService } from "./challenge.service";
+import { ChallengeController } from "./challenge.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Challenge, ChallengeSchema } from "./schemas/challenge.schema";
+import { ParseObjectIdPipe } from "../pipes/parse-object-id.pipe";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Challenge.name, schema: ChallengeSchema }])
   ],
-  providers: [ChallengeService],
+  providers: [ChallengeService, ParseObjectIdPipe],
   controllers: [ChallengeController]
 })
 export class ChallengeModule {}
