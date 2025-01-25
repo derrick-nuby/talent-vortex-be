e1d79c07202be58f561cca52936862b193ab436c
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-25 23:32:25 +0200

    ft: add category module into mongoose and app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MailModule } from "./mail/mail.module";
 import { AuthModule } from "./auth/auth.module";
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ ConfigModule, UserModule ],
+      imports: [ ConfigModule, UserModule, CategoryModule ],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
@@ -18,7 +19,8 @@ import { AuthModule } from "./auth/auth.module";
     }),
     UserModule,
     MailModule,
-    AuthModule
+    AuthModule,
+    CategoryModule
   ],
   controllers: [AppController],
   providers: [AppService],
