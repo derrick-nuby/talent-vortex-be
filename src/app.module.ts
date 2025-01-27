b4b66b5ca16093a5e809f83114a33faae390fb2f
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-27 18:45:13 +0200

    refactor: import form-module and remove unnecessary imports in Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UserModule } from "./user/user.module";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { MailModule } from "./mail/mail.module";
-import { AuthModule } from "./auth/auth.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { ChallengeModule } from './challenge/challenge.module';
+import { FormModule } from './form/form.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ ConfigModule, UserModule, CategoryModule, ChallengeModule ],
+      imports: [ ],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
@@ -22,7 +23,8 @@ import { ChallengeModule } from './challenge/challenge.module';
     MailModule,
     AuthModule,
     CategoryModule,
-    ChallengeModule
+    ChallengeModule,
+    FormModule
   ],
   controllers: [AppController],
   providers: [AppService],
