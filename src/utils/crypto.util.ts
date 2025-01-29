661130a57922766f92d06a59adf54e9e25a204ab
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-29 13:37:31 +0200

    refactor: fix CRYPTO_SECRET_KEY fetch from .env

--- src/utils/crypto.util.ts
+++ src/utils/crypto.util.ts
@@ -1,13 +1,20 @@
 import * as crypto from "crypto-js";
-
-const SECRET_KEY = process.env.CRYPTO_SECRET_KEY
+import * as process from "process";
 
 
 export function encrypt(text: string): string {
+  const SECRET_KEY = process.env.CRYPTO_SECRET_KEY
+  if (!text) {
+    throw new Error('Text to encrypt cannot be empty');
+  }
   return crypto.AES.encrypt(text, SECRET_KEY).toString();
 }
 
 export function decrypt(encryptedText: string): string {
+  const SECRET_KEY = process.env.CRYPTO_SECRET_KEY
+  if (!encryptedText) {
+    throw new Error('Text to decrypt cannot be empty');
+  }
   const bytes = crypto.AES.decrypt(encryptedText, SECRET_KEY);
   return bytes.toString(crypto.enc.Utf8);
 }
